02363ac7b6f99ce4387e5411193a0b357c565935
UselessAmber <mainrurikoproject@gmail.com>
2025-09-16 14:19:07 +1000

    requirements file updated

--- sle_backend_app/quickstart/engine/engine.py
+++ sle_backend_app/quickstart/engine/engine.py
@@ -1,6 +1,13 @@
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.files.storage import FileSystemStorage
 
+tmpfiles = "./tmpfiles/"
+
 def download_image_file(f : InMemoryUploadedFile):
     print(f"[upload] Download image")
-    FileSystemStorage(location = "./tmpfiles/").save(f.name, f)
+    FileSystemStorage(location = tmpfiles).save(f.name, f)
+
+def get_prediction(f : InMemoryUploadedFile):
+    download_image_file(f)
+    file_name = f.name
+        
